2a25d8feb063e1ffe40d9287a5d00512b574300a
skeletik101 <hdhde121212e@gmail.com>
2024-08-29 09:45:35 +0300

    Последний штрих

--- upload.sh
+++ upload.sh
@@ -13,8 +13,5 @@ unzip ./bin.zip
 cd ..
 chmod 777 ./bin/php7/bin/php
 chmod 777 ./start.sh
-rm -r ./start.php
-rm -r ./start.cmd
-rm -r ./start.ps1
 echo "[ОК] Сервер успешно установлен."
 echo "[INFO] Сервер успешно установлен."
